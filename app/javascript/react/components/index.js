0bc61b22b9479d3f02004e9f2d7034db4b84acad
OThayer <oliverthayer01@gmail.com>
2019-10-29 17:07:03 -0400

    tons of styling on index and established requests page

--- app/javascript/react/components/index.js
+++ app/javascript/react/components/index.js
@@ -29,6 +29,32 @@ const Index = props => {
         <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
         <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
       </div>
+
+    <div id="carousel" className="carousel slide carousel-fade" data-ride="carousel">
+      <div className="carousel-inner">
+        <div className="carousel-item active" data-interval="15000">
+            <img src="https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1873&q=80" className="d-block w-100" alt="..."/>
+            <div class="carousel-caption d-none d-md-block" id="slide-1">
+              <h3>Make Your Home Feel Like Home Again</h3>
+              <p>Habitate makes renting easy. Control your experience with the touch of a few keys</p>
+            </div>
+        </div>
+        <div className="carousel-item">
+          <img src="https://images.unsplash.com/photo-1534398079543-7ae6d016b86a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" className="d-block w-100" alt="..."/>
+          <div class="carousel-caption d-none d-md-block" id="slide-2">
+            <h3>Place maintenance requests right from your computer</h3>
+            <p>Forget always being sent to voicemail, place work requests for plumbers, electricians, and more.</p>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src="https://images.unsplash.com/photo-1563013544-824ae1b704d3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" className="d-block w-100" alt="..."/>
+          <div class="carousel-caption d-none d-md-block" id="slide-3">
+            <h3>Never Miss a Rent Payment Again</h3>
+            <p>With Stripe, setting up automated payments has never been easier.</p>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
