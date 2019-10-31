fc2cd2177c7f4e2a6405aea36f08336c2d7401a4
OThayer <oliverthayer01@gmail.com>
2019-10-31 14:45:23 -0400

    idk

---
+++ app/javascript/react/components/RequestsIndexContainer.js
@@ -0,0 +1,127 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+const RequestsIndexContainer = props => {
+  const [newRequest, setNewRequest] = useState({
+    work_type: "",
+    description: ""
+  })
+
+  const handleTypeClick = event => {
+    setNewRequest({
+      ...newRequest,
+      work_type: event.currentTarget.id
+    })
+  }
+
+  const handleChange = event => {
+  setNewRequest({
+    ...newRequest,
+    [event.currentTarget.name]: event.currentTarget.value
+  })
+ }
+
+ const onRequestSubmitted = (payload) => {
+    fetch(`/api/v1/requests`, {
+      method: 'POST',
+      credentials: "same-origin",
+      headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    })
+    .then(response => {
+      if (response.ok) {
+        return response;
+      } else {
+        throw new Error(`${response.status} ${response.statusText}`)
+      }
+    })
+    .then(body => {
+      return body.json()
+    })
+    .catch(error => console.error(`Error in fetch: ${error.message}`))
+    }
+
+    const onSubmitHandler = (event) => {
+      event.preventDefault()
+      onRequestSubmitted(payload)
+    }
+
+    let payload = {
+      work_type: newRequest.work_type,
+      description: newRequest.description
+    }
+
+    let maintenanceType = ""
+    if (newRequest.work_type == "plumbing") {
+      maintenanceType = "Looks like you need a plumber to fix..."
+    } else if (newRequest.work_type == "electric") {
+      maintenanceType = "Looks like you need an electrician to fix..."
+    } else if (newRequest.work_type == "HVAC") {
+      maintenanceType = "Looks like you need an HVAC Technician to fix..."
+    } else if (newRequest.work_type == "carpentry") {
+      maintenanceType = "Looks like you need a carpenter to fix..."
+    } else if (newRequest.work_type == "misc") {
+      maintenanceType = "Looks like you have a miscellaneous issue..."
+    }
+
+  return (
+
+    <div class="jumbotron-request">
+     <div id="maintenance-form">
+      <div className="container col-md-auto" id="new-form">
+        <div className="container" className="text-center">
+          <h2>Maintenance Request</h2>
+        </div>
+        <div className="cell">
+        </div>
+        <div className="container" id="type-buttons">
+          <form className="callout" onSubmit={onSubmitHandler}>
+            <label htmlFor="work-type" className="buttons">
+            <div id="button-holder">
+              <div className="buttons-hooray">
+                <button type="button" className="btn btn-primary" onClick={handleTypeClick} id="plumbing">
+                  <i className="fa fa-shower"></i>
+                </button>
+              </div>
+              <div className="buttons-hooray">
+                <button type="button" className="btn btn-primary" onClick={handleTypeClick} id="electric">
+                  <i className="fas fa-lightbulb"></i>
+                </button>
+              </div>
+              <div className="buttons-hooray">
+              <button type="button" className="btn btn-primary" onClick={handleTypeClick} id="HVAC">
+                <i className="fas fa-thermometer-half" id="thermometer"></i>
+              </button>
+              </div>
+              <div className="buttons-hooray">
+              <button type="button" className="btn btn-primary" onClick={handleTypeClick} id="carpentry">
+                <i className="fas fa-hammer" id="hammer"></i>
+              </button>
+              </div>
+              <div className="buttons-hooray">
+              <button type="button" className="btn btn-primary" onClick={handleTypeClick} id="misc">
+                <i className="fas fa-question-circle" id="misc"></i>
+              </button>
+              </div>
+             </div>
+          </label>
+          <h4 id="maintenance-message">{maintenanceType}</h4>
+          <div id="text-field">
+            <label htmlFor="description" id="description" value="description" >
+              <textarea placeholder="Please leave a detailed description of what needs fixed" type="text" id="description" name="description" cols="70" onChange={handleChange}/>
+              <input className="btn btn-primary" type="submit" value="Submit"/>
+            </label>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  </div>
+  )
+}
+
+
+export default RequestsIndexContainer
--- app/javascript/react/components/index.js
+++ app/javascript/react/components/index.js
@@ -22,13 +22,39 @@ const Index = props => {
 
   return (
     <div>
-      <div className="jumbotron">
+      <div className="jumbotron-index">
         <h1 className="display-4">Hello {currentUser.first_name}!</h1>
         <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
         <hr className="my-4"/>
         <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
       </div>
+
+    <div id="carousel" className="carousel slide carousel-fade" data-ride="carousel">
+      <div className="carousel-inner">
+        <div className="carousel-item active" data-interval="10000">
+            <img src="https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1873&q=80" className="d-block w-100" alt="..."/>
+            <div class="carousel-caption d-none d-md-block" id="slide-1">
+              <h3>Make Your Home Feel Like Home Again</h3>
+              <p>Habitate makes renting easy. Control your experience with the touch of a few keys</p>
+            </div>
+        </div>
+        <div className="carousel-item">
+          <img src="https://images.unsplash.com/photo-1534398079543-7ae6d016b86a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" className="d-block w-100" alt="..."/>
+          <div class="carousel-caption d-none d-md-block" id="slide-2">
+            <h3>Place maintenance requests right from your computer</h3>
+            <p>Forget always being sent to voicemail, place work requests for plumbers, electricians, and more.</p>
+            <a className="btn btn-primary btn-lg" href="/MaintenanceInfoPage" role="button">Learn more</a>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src="https://images.unsplash.com/photo-1563013544-824ae1b704d3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" className="d-block w-100" alt="..."/>
+          <div class="carousel-caption d-none d-md-block" id="slide-3">
+            <h3>Never Miss a Rent Payment Again</h3>
+            <p>With Stripe, setting up automated payments has never been easier.</p>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
