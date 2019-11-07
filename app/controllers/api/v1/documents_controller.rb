b6a7ca5271f4f8ca282308a9cd5f9906e17c36a5
OThayer <oliverthayer01@gmail.com>
2019-11-07 16:37:10 -0500

    added early stages of document upload

---
+++ app/controllers/api/v1/documents_controller.rb
@@ -0,0 +1,24 @@
+class Api::V1::DocumentsController < ApiController
+
+  def index
+    tenant = current_user
+    documents = tenant.documents
+    render json: documents
+  end
+
+  def create
+    document = Document.new(
+      title: params[:title],
+      body: params[:body]
+    )
+    document.tenant = current_user
+    if document.save
+        render json: document
+      else
+        render json: {
+          errors: document.errors.messages,
+          fields: document
+        }
+      end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [ :index ]
       resources :requests, only: [ :index, :create, :show ]
+      resources :documents, only: [ :index, :create ]
     end
   end
 
